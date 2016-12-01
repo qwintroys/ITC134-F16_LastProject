d7eec43a9731dc72bbc8b6ee296106a33d39357b
qwintroys <qwintroys@gmail.com>
2016-12-01 13:06:50 -0800

    removed double quote replace with single quotes
    removed double quote replace with single quotes

--- RemoteConnect.php
+++ RemoteConnect.php
@@ -4,7 +4,7 @@ class RemoteConnect
   public function connectToServer($serverName=null)
   {
     if($serverName==null){
-      throw new Exception(“That is not a server name!”);
+      throw new Exception('That is not a server name!');
     }
     $fp = fsockopen($serverName,80);
     return ($fp) ? true : false;
